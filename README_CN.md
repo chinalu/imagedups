1f62563fcfdc2ce991e5028cc654abc55c591556
Bill <luyiping@yiwei.com>
2020-10-11 21:06:40 +0800

    Chinese version README

---
+++ README_CN.md
@@ -0,0 +1,41 @@
+# imagedups
+查找/删除相同的图片文件
+
+## 开发背景
+
+在进行图片分类的机器学习时，收集整理数据是一个必不可少的步骤。但是在使用fdupes进行文件去重时，发现很多看起来一样的图片，实际上二进制文件是不一样的。所以开发一个小程序，根据phash，计算图片的average_hash是否一致。如果一致，则认为文件是相同的。 
+
+## 安装 
+python setup.py install
+
+## 使用方法
+
+1. 仅显示重复的图片:
+
+   ```shell
+   imagedups -p /path/to/image/folder1  
+   ```
+
+   [+]开头的文件是保留的文件；
+
+   [-]开头的文件是可以清理的文件。
+
+2. 递归查找文件：
+
+   ```shell
+   imagedups -r -p /path/to/image/folder
+   ```
+
+3. 查找并删除文件:
+
+   ```shell
+   imagedups -r -d -p /path/to/image/folder
+   ```
+
+4. 删除文件是不进行确认:
+
+   ```shell
+   imagedups -r -d -N -p /path/to/image/folder
+   ```
+
+   请做好数据备份
--- src/imagedups.py
+++ src/imagedups.py
@@ -63,51 +63,6 @@ def dupes(config):
     
 
 def main(args=None):
-    '''
-    Usage: fdupes [options] DIRECTORY...
-
- -r --recurse            for every directory given follow subdirectories
-                         encountered within
- -R --recurse:           for each directory given after this option follow
-                         subdirectories encountered within (note the ':' at the
-                         end of the option, manpage for more details)
- -s --symlinks           follow symlinks
- -H --hardlinks          normally, when two or more files point to the same
-                         disk area they are treated as non-duplicates; this
-                         option will change this behavior
- -G --minsize=SIZE       consider only files greater than or equal to SIZE bytes
- -L --maxsize=SIZE       consider only files less than or equal to SIZE bytes
- -n --noempty            exclude zero-length files from consideration
- -A --nohidden           exclude hidden files from consideration
- -f --omitfirst          omit the first file in each set of matches
- -1 --sameline           list each set of matches on a single line
- -S --size               show size of duplicate files
- -t --time               show modification time of duplicate files
- -m --summarize          summarize dupe information
- -q --quiet              hide progress indicator
- -d --delete             prompt user for files to preserve and delete all
-                         others; important: under particular circumstances,
-                         data may be lost when using this option together
-                         with -s or --symlinks, or when specifying a
-                         particular directory more than once; refer to the
-                         fdupes documentation for additional information
- -P --plain              with --delete, use line-based prompt (as with older
-                         versions of fdupes) instead of screen-mode interface
- -N --noprompt           together with --delete, preserve the first file in
-                         each set of duplicates and delete the rest without
-                         prompting the user
- -I --immediate          delete duplicates as they are encountered, without
-                         grouping into sets; implies --noprompt
- -p --permissions        don't consider files with different owner/group or
-                         permission bits as duplicates
- -o --order=BY           select sort order for output and deleting; by file
-                         modification time (BY='time'; default), status
-                         change time (BY='ctime'), or filename (BY='name')
- -l --log=LOGFILE        log file deletion choices to LOGFILE
- -v --version            display fdupes version
- -h --help               display this help message
-
-    '''
     parser = argparse.ArgumentParser(
         prog="imagedups",
         description="""Find/Delete duplicated images
